bcb2945dd8205c1f1c9a5e9f9262620f41ebd545
porkbeans <mizuo.taka@gmail.com>
2021-02-14 22:08:39 +0900

    Disable selenium middleware for performance issue

--- netkeiba/settings.py
+++ netkeiba/settings.py
@@ -49,9 +49,9 @@ DOWNLOAD_DELAY = 0.2
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-    'netkeiba.middlewares.SeleniumDownloaderMiddleware': 800,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#     'netkeiba.middlewares.SeleniumDownloaderMiddleware': 800,
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
